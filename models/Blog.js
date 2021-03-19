784fd99e4afdfdde27941bb9479adb8048ee3128
Lita Beach <litabeach123@gmail.com>
2021-03-19 12:10:38 -0500

    blog model created

---
+++ models/Blog.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model{
+
+}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+//   the fields createdAt and updatedAt are automatically added to every model in sequelize. Timestamps are turned on
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = Blog;
