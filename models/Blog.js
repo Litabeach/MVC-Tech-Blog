e718e8851dea73af037b3ae8f41d302c5d08702e
Lita Beach <litabeach123@gmail.com>
2021-03-19 12:13:45 -0500

    added foreign key to blog, updated index.js mdoel to show relationship btwn user and blog

--- models/Blog.js
+++ models/Blog.js
@@ -1,35 +1,42 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model{
+class Blog extends Model {
 
 }
 
 Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        //   the fields createdAt and updatedAt are automatically added to every model in sequelize. Timestamps are turned on
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-//   the fields createdAt and updatedAt are automatically added to every model in sequelize. Timestamps are turned on
-  },
-  {
-    sequelize,
-    timestamps: true,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
 );
 
 module.exports = Blog;
