07e22efa7fe21aadb1581ab285b6ac2bad831cdc
Lita Beach <litabeach123@gmail.com>
2021-03-22 12:38:12 -0500

    login/signup routes working

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,37 @@
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            where: {
+                user_id: req.session.user_id,
+            },
+            attributes: ["id", "title", "description", "createdAt"],
+            // include: [
+            // { model: User, 
+            // attributes: ["name"] },
+            //     include: {
+            //         model: User, 
+            //         attributes: ['name'],
+            //     }
+            // }]
+        })
+        
+        console.log("TEST" + blogData)
+        if (!blogData) {
+            res.status(404).json({
+                message: "No post found with this id"
+            });
+            return;
+        }
+  
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+       console.log(blogs)
+  
+        res.render('dashboard', {
+            blogs,
+            loggedIn: req.session.logged_in
+        })
+  
+    } catch (err) {
+        res.status(500).json(err)
+    }
+  })
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./api/userRoutes');
-const blogRoutes = require('./blogRoutes');
+const blogRoutes = require('./api/blogRoutes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/users', userRoutes);
+router.use('/', homeRoutes);
 router.use('/blogs', blogRoutes);
 
 module.exports = router;
