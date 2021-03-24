ad2aaa0a23ee840a658a08a9fa772d89ae11490d
Lita Beach <litabeach123@gmail.com>
2021-03-24 16:56:19 -0500

    added field and submit button for comments in html

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,10 +2,15 @@
 function createNewElement() {
     // First create a DIV element.
 	var txtNewInputBox = document.createElement('div');
+    var txtNewButton = document.createElement('div');
 
     // Then add the content (a new input box) of the element.
 	txtNewInputBox.innerHTML = "<input type='text' id='newInputBox'>";
 
+      // Then add the content (a new input box) of the element.
+	txtNewButton.innerHTML = "<button id='newButton'>Submit</button>";
+
     // Finally put it where it is supposed to appear.
 	document.getElementById("newElementId").appendChild(txtNewInputBox);
+    document.getElementById("newElementId").appendChild(txtNewButton);
 }
